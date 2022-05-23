dae21863e09c517591d83ba4a1ed675dfbed8ebd
Kiril <alexbour82@gmail.com>
2022-05-23 18:05:05 -0300

    index update

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,29 +1,26 @@
 import { motion } from 'framer-motion'
-import me from '../static/images/me.png'
 import Hider from "./Hider"
+import Image from './index/image'
+import Examples from './index/Example'
 
 export default function Idex() {
-  
-return (
-  <Hider>
-    <div className='flex flex-col'>
-      {/* main content */}
-      <div className='flex xsm:flex-col mx-auto mt-20 xsm:mt-5 xsm:mx-3 text-white'>
-        <div className='my-auto'>
-          <p className="text-4xl">Hi, I'm Kiril Bourakov.</p> <br/>
-          <p className="text-xl">I design modern, responsive websites using React and other technolagies.</p>
-        </div> 
-        <div className='relative'>
-          <div className='overflow-hidden rounded-full self-center'>
-            <img src={me} />
+  return (
+    <Hider>
+      <div className='flex flex-col'>
+        {/* main content */}
+        <div className='flex xsm:flex-col mt-20 xsm:mt-5 xsm:mx-3 text-white'>
+          <div className='my-auto mx-16'>
+            <p className="text-4xl">Hi, I'm Kiril Bourakov.</p> <br/>
+            <p className="text-xl">I design modern, responsive websites using React and other technolagies.</p>
           </div>
-          <div className='absolute right-0 bottom-0 cursor-default'>
-            <p>Source: vecteezy.com</p>
-          </div>
+          <Image/> 
+        </div>
+        {/* links */}
+        <div className='flex ml-16'>
+          <Examples to={'github'} title={'Github'}/>
+          <Examples to={'projects'} title={'Projects'}/>
         </div>
       </div>
-      {/* links */}
-    </div>
-  </Hider>
-  )
-}
+    </Hider>
+    )
+  }
