084f2872674a7079b1339807568b3b25774b0a49
Kiril <alexbour82@gmail.com>
2022-05-25 17:10:27 -0300

    added animations

--- src/components/Hider.jsx
+++ src/components/Hider.jsx
@@ -1,12 +1,14 @@
+import { AnimatePresence } from 'framer-motion'
 import { UseNavContext } from '../hooks/NavHook'
+
 
 export default function Hider({ children }) {
     const SeeNav = UseNavContext()
     return(
-        <>
+        <AnimatePresence>
         {!SeeNav &&
             children
         }    
-        </>
+        </AnimatePresence>
     )
 }
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import Project from "./projects/project"
 import WarframeSite from '../static/images/WarframeSite.png'
 import UsStats from '../static/images/USstats.png'
@@ -7,27 +7,29 @@ import Hider from "./Hider"
 export default function Projects() {
     return(
         <Hider>
-        <motion.div 
-        initial={{ opacity:0 }}
-        animate={{ opacity:1 }}
-        className="h-screen-without-nav bg-red-500 flex flex-col">
-            <Project 
-                title={'Warframe API Parser'}
-                description={'This project parses the warframe api using it to produce a website.'}
-                image={WarframeSite}
-                Sitelink={'https://warframeparser.netlify.app/'}
-                Githublink={'https://github.com/thepug432/WarframeApiParser'}
-                features={['Minimize Data', 'Hide Data', 'Change Platforms (Stored Locally)', 'Error Checking On Data Fetching', 'Loading Screen While Fetching Data', 'Responsive']}
-            />
-            <Project
-                title={'United States Population Tracker'}
-                description={'This website tracks the united states population with a chart throught the years.'}
-                image={UsStats}
-                Sitelink={'https://uspopulationtracker.netlify.app/'}
-                Githublink={'https://github.com/thepug432/USPopulation'}
-                features={['Dynamic Chart Creation Via API', 'Data For United States', 'Data By State', 'Responsive']}
-            />
-        </motion.div>
+            <motion.div 
+            initial={{ opacity:0 }}
+            exit={{ opacity:0 }}
+            animate={{ opacity:1 }}
+            transition={{ duration:.3 }}
+            className="h-screen-without-nav bg-red-500 flex flex-col">
+                <Project 
+                    title={'Warframe API Parser'}
+                    description={'This project parses the warframe api using it to produce a website.'}
+                    image={WarframeSite}
+                    Sitelink={'https://warframeparser.netlify.app/'}
+                    Githublink={'https://github.com/thepug432/WarframeApiParser'}
+                    features={['Minimize Data', 'Hide Data', 'Change Platforms (Stored Locally)', 'Error Checking On Data Fetching', 'Loading Screen While Fetching Data', 'Responsive']}
+                />
+                <Project
+                    title={'United States Population Tracker'}
+                    description={'This website tracks the united states population with a chart throught the years.'}
+                    image={UsStats}
+                    Sitelink={'https://uspopulationtracker.netlify.app/'}
+                    Githublink={'https://github.com/thepug432/USPopulation'}
+                    features={['Dynamic Chart Creation Via API', 'Data For United States', 'Data By State', 'Responsive']}
+                />
+            </motion.div>
         </Hider>
     )
 }
