10a1b4a419af346699cc7d115281deed3df85ca2
Kiril <alexbour82@gmail.com>
2022-05-25 20:26:18 -0300

    resume education update

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,16 +1,25 @@
 import Education from "./resume/education"
-import CS50X from '../static/images/CS50X.png'
+import cS50X from '../static/images/CS50X.png'
+import webdev from '../static/images/webdev.jpeg'
 
 export default function Resume() {
     return(
         <div className="w-full h-screen">
             <div className="flex flex-col">
                 <Education 
+                    title={'Professional Certificate Computer Science For Web Programming'}
+                    titleaddon={'(From HarvardX)'}
+                    institution={'HarvardX (on EDX)'}
+                    discription={'This is a professional certificate given out by Harvard university for the verified completion of its computer science for web programming courses.'}
+                    image={webdev}
+                    url={'https://credentials.edx.org/credentials/e70dbbc4f448422fa496e5650766fc45/'}
+                />
+                <Education 
                     title={'CS50X'}
                     institution={'HarvardX (on EDX)'}
                     discription={'This is an introduction to the intellectual enterprises of computer science and the art of programming. The course covers C, SQL, Python, Javascript, HTML & CSS, and Web Development using Flask.'}
                     grade={'Completed with Verified Certificate'}
-                    image={CS50X}
+                    image={cS50X}
                     url={'https://courses.edx.org/certificates/390fbc6b10a34b0e8fef28b735cad7f1'}
                 />
             </div>
--- src/components/resume/education.jsx
+++ src/components/resume/education.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import HR from './hr'
 import HoverEffect from './HoverEffect'
 
-export default function Education({url, title, institution, discription, grade, date, image}) {
+export default function Education({url, title, institution, discription, grade, date, image, titleaddon}) {
     const [hover, sethover] = useState(false)
 
     return(
@@ -10,9 +10,14 @@ export default function Education({url, title, institution, discription, grade, 
             <div 
             onMouseEnter={() => sethover(true)}
             onMouseLeave={() => sethover(false)}
-            className="flex xsm:flex-col text-white border-b border-white border-solid absolute top-0 left-0">
+            className="flex flex-col text-white border-b border-white border-solid">
+
+                {/* title */}
+                <h1 className="text-3xl mx-auto xsm:text-center"><strong>{title}</strong> {titleaddon}</h1>
+
+                {/* side */}
+                <div className='flex'>
                 <div className="pl-2 self-center">
-                    <h1 className="text-3xl mx-auto xsm:text-center">{title}</h1>
                     <h2 className="text-xl mx-auto xsm:text-center">{institution}</h2>
                     {grade &&
                         <div className="self-center ml-auto mr-10 xsm:mx-auto">
@@ -21,14 +26,17 @@ export default function Education({url, title, institution, discription, grade, 
                     }
                 </div>
                 <HR/>
-                <div className="self-center xsm:self-center">
-                    <p className="xsm:text-center">{discription}</p>
+
+                {/* body */}
+                <div className="self-center xsm:self-center mx-auto p-5">
+                    <p className="xsm:text-center max-w-lg">{discription}</p>
                 </div>
                 <HR/>
-                <div className="p-2 xsm:self-center">
-                    <img src={image}></img>
+                <div className="p-2 my-auto self-center xsm:hidden">
+                    <img src={image} className='object-scale-down h-48 w-96'></img>
                 </div>
                 <HoverEffect hover={hover} url={url}/>
+                </div>
             </div>
         </div>
     )
